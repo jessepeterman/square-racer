da913d75c473ddbcc696bef9f6563c47990f12b3
Jesse Peterman <jesse@Jesses-iMac.local>
2018-05-20 09:27:23 -0700

    trying viewport option again

--- docs/app.js
+++ docs/app.js
@@ -43,35 +43,8 @@ function resetPlayerPositions(){
 
 updateScores();
 
-p1Btn.addEventListener("touchstart", preventZoom);
-
-// p1Btn.addEventListener('click', (e) => moveRight(player1));
+p1Btn.addEventListener('click', (e) => moveRight(player1));
 p2Btn.addEventListener('click', (e) => moveRight(player2));
-
-function preventZoom(e) {
-  var t2 = e.timeStamp;
-  var t1 = e.currentTarget.dataset.lastTouch || t2;
-  var dt = t2 - t1;
-  var fingers = e.touches.length;
-  e.currentTarget.dataset.lastTouch = t2;
-
-  if (!dt || dt > 500 || fingers > 1) return; // not double-tap
-
-  e.preventDefault();
-  e.target.click();
-  moveRight(player1);
-}
-
-
-// p1Btn.addEventListener('touchend', (e) => {
-//   e.preventDefault();
-//   e.target.click();
-// });
-
-// p2Btn.addEventListener('touchend', (e) => {
-//   e.preventDefault();
-//   this.click();
-// });
 
 resetBtn.addEventListener('click', (e) => resetScores());
 
