9c454725b8c2446e28bae6dcc1aee145e7c4ce4b
Jesse Peterman <jesse@Jesses-iMac.local>
2018-05-20 08:58:57 -0700

    changed mobile style to remove keyboard controls, testing out prevent zoom function

--- docs/app.js
+++ docs/app.js
@@ -44,7 +44,23 @@ function resetPlayerPositions(){
 updateScores();
 
 p1Btn.addEventListener('click', (e) => moveRight(player1));
+p1Btn.addEventListener('touchstart', (e) => preventZoom);
 p2Btn.addEventListener('click', (e) => moveRight(player2));
+p2Btn.addEventListener('touchstart', (e) => preventZoom);
+
+function preventZoom(e) {
+  var t2 = e.timeStamp;
+  var t1 = e.currentTarget.dataset.lastTouch || t2;
+  var dt = t2 - t1;
+  var fingers = e.touches.length;
+  e.currentTarget.dataset.lastTouch = t2;
+
+  if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+  e.preventDefault();
+  e.target.click();
+}
+
 
 // p1Btn.addEventListener('touchend', (e) => {
 //   e.preventDefault();
